c3ea3b5a045e3659bab2171d26a5ba0aa7e26985
JCBreath <Mimafama0@qq.com>
2018-02-04 20:52:03 -0800

    Update MouseHook.cs

--- MouseHook.cs
+++ MouseHook.cs
@@ -23,17 +23,17 @@ namespace ECS188
         private const int WM_RBUTTONDBLCLK = 0x206;
         private const int WM_MBUTTONDBLCLK = 0x209;
 
-        //全局的事件  
+        
         public event MouseEventHandler OnMouseActivity;
 
-        static int hMouseHook = 0; //鼠标钩子句柄  
+        static int hMouseHook = 0; 
 
-        //鼠标常量  
+         
         public const int WH_MOUSE_LL = 14; //mouse hook constant  
 
-        HookProc MouseHookProcedure; //声明鼠标钩子事件类型.  
+        HookProc MouseHookProcedure;
 
-        //声明一个Point的封送类型  
+        
         [StructLayout(LayoutKind.Sequential)]
         public class POINT
         {
@@ -41,7 +41,7 @@ namespace ECS188
             public int y;
         }
 
-        //声明鼠标钩子的封送结构类型  
+        
         [StructLayout(LayoutKind.Sequential)]
         public class MouseHookStruct
         {
@@ -51,28 +51,26 @@ namespace ECS188
             public int dwExtraInfo;
         }
 
-        //装置钩子的函数  
+        
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
 
-        //卸下钩子的函数  
+         
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern bool UnhookWindowsHookEx(int idHook);
 
-        //下一个钩挂的函数  
+         
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
 
         public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
 
-        /// <summary>  
-        /// 墨认的构造函数构造当前类的实例.  
-        /// </summary>  
+     
         public MouseHook()
         {
         }
 
-        //析构函数.  
+      
         ~MouseHook()
         {
             Stop();
@@ -80,15 +78,15 @@ namespace ECS188
 
         public void Start()
         {
-            //安装鼠标钩子  
+            
             if (hMouseHook == 0)
             {
-                //生成一个HookProc的实例.  
+            
                 MouseHookProcedure = new HookProc(MouseHookProc);
 
                 hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProcedure, Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]), 0);
 
-                //如果装置失败停止钩子  
+               
                 if (hMouseHook == 0)
                 {
                     Stop();
@@ -106,13 +104,13 @@ namespace ECS188
                 hMouseHook = 0;
             }
 
-            //如果卸下钩子失败  
+            
             if (!(retMouse)) throw new Exception("UnhookWindowsHookEx failed.");
         }
 
         private int MouseHookProc(int nCode, Int32 wParam, IntPtr lParam)
         {
-            //如果正常运行并且用户要监听鼠标的消息  
+             
             if ((nCode >= 0) && (OnMouseActivity != null))
             {
                 MouseButtons button = MouseButtons.None;
@@ -146,10 +144,10 @@ namespace ECS188
 
                 }
 
-                //从回调函数中得到鼠标的信息  
+                 
                 MouseHookStruct MyMouseHookStruct = (MouseHookStruct)Marshal.PtrToStructure(lParam, typeof(MouseHookStruct));
                 MouseEventArgs e = new MouseEventArgs(button, clickCount, MyMouseHookStruct.pt.x, MyMouseHookStruct.pt.y, 0);
-                //if(e.X>700)return 1;//如果想要限制鼠标在屏幕中的移动区域可以在此处设置  
+                
                 OnMouseActivity(this, e);
             }
             return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
